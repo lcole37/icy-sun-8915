c61d7ef854836a5350fb4e1356da04ff1cfb04f1
Lucas Colwell <colwellc37@gmail.com>
2022-07-25 10:23:00 -0600

    Test: story 1

--- spec/features/departments/index_spec.rb
+++ spec/features/departments/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'depts index page' do
-  it "shows all departments" do
+  it "shows all department names" do
     appliances = Department.create!(name: "Appliances", floor: "Basement")
     lighting = Department.create!(name: "Lighting", floor: "Basement")
     garden = Department.create!(name: "Garden", floor: "One")
@@ -12,5 +12,34 @@ RSpec.describe 'depts index page' do
     expect(page).to have_content("Lighting")
     expect(page).to have_content("Garden")
     expect(page).not_to have_content("toys")
+  end
+
+  it "shows department floor, and employees" do
+    appliances = Department.create!(name: "Appliances", floor: "Basement")
+    lighting = Department.create!(name: "Lighting", floor: "Basement")
+    garden = Department.create!(name: "Garden", floor: "One")
+
+    ally = appliances.employees.create!(name: "Ally", level: 1)
+    bob = appliances.employees.create!(name: "Bob", level: 1, department_id: 1)
+    chris = lighting.employees.create!(name: "Chris", level: 1, department_id: 2)
+    molly = garden.employees.create!(name: "Molly", level: 2, department_id: 3)
+
+    visit "/departments"
+
+    # within "#departments-#{appliances.id}" do
+      expect(page).to have_content("Ally")
+      expect(page).to have_content("Bob")
+      expect(page).to have_content("Basement")
+    # end
+
+    # within "#departments-#{lighting.id}" do
+      expect(page).to have_content("Chris")
+      expect(page).to have_content("Basement")
+    # end
+
+    # within "#departments-#{garden.id}" do
+      expect(page).to have_content("Molly")
+      expect(page).to have_content("One")
+    # end
   end
 end
